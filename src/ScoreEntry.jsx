9d039106f7efc001571862a64f051b02ef15b364
morteako <morteako@student.matnat.uio.no>
2019-09-24 20:20:15 +0200

    la til rød tekst for <7 dager rekord

--- src/ScoreEntry.jsx
+++ src/ScoreEntry.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./mystyle.module.css";
+import diff from "jest-diff";
 
 export const createScoreEntry = (data,clicked,ind) => {
     const props = {...data,clicked};
@@ -18,6 +19,15 @@ const secToMMSS = timeInSecStr => {
     date.setSeconds( secs );
     return date.toISOString().substr(14, 5);
 }
+
+const isSetPastWeek = date => {
+    
+    const diffTime = Math.abs(new Date() - new Date(date));
+    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
+    // console.log(date,new Date(date),diffTime,diffDays);
+    return diffDays < 7;
+}
+
 const ScoreEntry = props => {
     const {athlete_name, elapsed_time, start_date_local,rank,clicked} = props;
     const date = start_date_local.substr(0,10).split("-").reverse().join(".");
@@ -29,10 +39,14 @@ const ScoreEntry = props => {
         2:styles.entry_second,
         3:styles.entry_third
     };
-    const className = entryClasses[rank] || styles.entry_normal;
+    const scoreClass = entryClasses[rank] || styles.entry_normal;
+    // console.log(scoreClass);
+    const wasSetPastWeek = isSetPastWeek(start_date_local);
+    // if(wasSetPastWeek) console.log(date);
 
+    const borderClass = wasSetPastWeek ? ` ${styles.entry_border}` : "";
     return (
-        <td className={className}> 
+        <td className={scoreClass + borderClass}> 
             {text} 
             {' '}
             <span>{clicked && date}</span>
