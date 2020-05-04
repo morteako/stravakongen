d811a568ae0f14643e4a4dfc6581368e2849f651
Morten Kolstad <morteako@student.matnat.uio.no>
2020-05-04 19:28:49 +0200

    Revert "Revert "ranmed ranking""
    This reverts commit 854564ccca426953bf036f7ae52ee6ceb3083225.

--- src/calculation/ranking.js
+++ src/calculation/ranking.js
@@ -14,7 +14,7 @@ const fixSharedPosition = rankings => {
   return fixedRankPos;
 };
 
-const getRanking = (allTime, segments, leaderboards) => {
+const rankLowestPoints = (allTime, segments, leaderboards) => {
   const getNoEffortScore = segId => leaderboards[segId].length + 1;
 
   const getRank = (effort, segmentId) => {
@@ -43,4 +43,4 @@ const getRanking = (allTime, segments, leaderboards) => {
   return fixSharedPosition(summedArray);
 };
 
-export default getRanking;
+export default rankLowestPoints;
