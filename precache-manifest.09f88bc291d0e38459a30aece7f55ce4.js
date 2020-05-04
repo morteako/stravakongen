99cc559b4b3be9346efee1c6e05c1dc421813610
morteako <morteako@student.matnat.uio.no>
2020-05-04 18:20:44 +0000

    Deploying to gh-pages from master 7bf1c515e96bfec46bb1c963ef799e5909fc556e

---
+++ precache-manifest.09f88bc291d0e38459a30aece7f55ce4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bacf2b8f4cdb6120ce7c484ef3576348",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68d5ea899eacd12fb66a",
+    "url": "/static/css/main.a98a2623.chunk.css"
+  },
+  {
+    "revision": "aeb7656a708891999f57",
+    "url": "/static/js/2.c1a45967.chunk.js"
+  },
+  {
+    "revision": "f07bd4d76e66b5c629584ed9e5d54b00",
+    "url": "/static/js/2.c1a45967.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68d5ea899eacd12fb66a",
+    "url": "/static/js/main.d243acbc.chunk.js"
+  },
+  {
+    "revision": "fad99ab895ec86519f54",
+    "url": "/static/js/runtime-main.fe26c9eb.js"
+  },
+  {
+    "revision": "217c0af596911b2c9df7ad5fb7340c4d",
+    "url": "/static/media/Elianto-Regular.217c0af5.ttf"
+  }
+]);
